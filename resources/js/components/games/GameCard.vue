82d6981ad4cd214cf1a23c8b4f61b9ce3a8642cd
felix-droese <felix.droese@clockin.de>
2025-04-05 21:10:55 +0200

    wip

--- resources/js/components/games/GameCard.vue
+++ resources/js/components/games/GameCard.vue
@@ -179,26 +179,28 @@ watch(
         <div v-if="props.game.result" class="mt-6">
             <div class="w-fit rounded-lg border bg-card p-4 text-sm shadow-sm">
                 <div class="grid grid-cols-[120px_40px_40px_40px] gap-x-4 gap-y-1">
-                    <div class="col-span-1 font-medium text-black text-sm">Result</div>
+                    <div class="col-span-1 text-sm font-medium text-black">Result</div>
                     <div v-for="i in 3" :key="i" class="text-center text-xs font-medium uppercase tracking-wider text-muted-foreground">
                         Set {{ i }}
                     </div>
 
-                    <div class="font-medium mt-4" :class="{ 'text-green-600': props.game.winning_team?.id === props.game.first_team.id }">First Team</div>
+                    <div class="mt-4 font-medium" :class="{ 'text-green-600': props.game.winning_team?.id === props.game.first_team.id }">
+                        First Team
+                    </div>
                     <div
-                        class="text-center font-medium mt-4"
+                        class="mt-4 text-center font-medium"
                         :class="{ 'text-green-600': props.game.result.sets[0].first_team > props.game.result.sets[0].second_team }"
                     >
                         {{ props.game.result.sets[0].first_team }}
                     </div>
                     <div
-                        class="text-center font-medium mt-4"
+                        class="mt-4 text-center font-medium"
                         :class="{ 'text-green-600': props.game.result.sets[1].first_team > props.game.result.sets[1].second_team }"
                     >
                         {{ props.game.result.sets[1].first_team }}
                     </div>
                     <div
-                        class="text-center font-medium mt-4"
+                        class="mt-4 text-center font-medium"
                         :class="{ 'text-green-600': props.game.result.sets[2].first_team > props.game.result.sets[2].second_team }"
                     >
                         {{ props.game.result.sets[2].first_team }}
