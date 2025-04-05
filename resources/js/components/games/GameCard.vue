8f29a51de6eff0602bf29fbd450f354655d3028d
felix-droese <felix.droese@clockin.de>
2025-04-05 21:12:16 +0200

    wip

--- resources/js/components/games/GameCard.vue
+++ resources/js/components/games/GameCard.vue
@@ -178,14 +178,15 @@ watch(
         </div>
         <div v-if="props.game.result" class="mt-6">
             <div class="w-fit rounded-lg border bg-card p-4 text-sm shadow-sm">
-                <div class="grid grid-cols-[120px_40px_40px_40px] gap-x-4 gap-y-1">
+                <div class="grid grid-cols-[200px_40px_40px_40px] gap-x-4 gap-y-1">
                     <div class="col-span-1 text-sm font-medium text-black">Result</div>
                     <div v-for="i in 3" :key="i" class="text-center text-xs font-medium uppercase tracking-wider text-muted-foreground">
                         Set {{ i }}
                     </div>
 
                     <div class="mt-4 font-medium" :class="{ 'text-green-600': props.game.winning_team?.id === props.game.first_team.id }">
-                        First Team
+                        {{ props.game.first_team.players[0].last_name
+                        }}{{ props.game.first_team.players[1] ? `/${props.game.first_team.players[1].last_name}` : '' }}
                     </div>
                     <div
                         class="mt-4 text-center font-medium"
@@ -207,7 +208,8 @@ watch(
                     </div>
 
                     <div class="font-medium" :class="{ 'text-green-600': props.game.winning_team?.id === props.game.second_team?.id }">
-                        Second Team
+                        {{ props.game.second_team?.players[0]?.last_name
+                        }}{{ props.game.second_team?.players[1] ? `/${props.game.second_team.players[1].last_name}` : '' }}
                     </div>
                     <div
                         class="text-center font-medium"
