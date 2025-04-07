65fa982c11f3e8716a6bb2b0408da0bbdfebd813
felix-droese <felix.droese@clockin.de>
2025-04-07 19:54:33 +0200

    wip

--- resources/js/components/games/card/GameMetadata.vue
+++ resources/js/components/games/card/GameMetadata.vue
@@ -12,13 +12,13 @@ const props = defineProps<{
         <div class="flex items-center gap-1">
             <Clock class="h-3 w-3 sm:h-4 sm:w-4" />
             <span>
-                {{ new Date(game.date).toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' }) }}
-                {{ new Date(game.date).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' }) }}
+                {{ new Date(props.game.date).toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' }) }}
+                {{ new Date(props.game.date).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', hour12: false }) }}
             </span>
         </div>
         <div class="flex items-center gap-1">
             <MapPin class="h-3 w-3 sm:h-4 sm:w-4" />
-            <span>{{ locations.find((l) => l.id === game.location_id)?.name }}</span>
+            <span>{{ props.locations.find((l) => l.id === props.game.location_id)?.name }}</span>
         </div>
     </div>
 </template>
