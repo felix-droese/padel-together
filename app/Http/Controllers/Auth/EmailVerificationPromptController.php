a66f9a7943a59a7a90c651671243d43a7c6f4269
felix-droese <felix.droese@clockin.de>
2025-04-08 16:24:17 +0200

    wip

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -16,7 +16,7 @@ class EmailVerificationPromptController extends Controller
     public function __invoke(Request $request): RedirectResponse|Response
     {
         return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(routes('games.index', absolute: false))
+                    ? redirect()->intended(route('games.index', absolute: false))
                     : Inertia::render('auth/VerifyEmail', ['status' => $request->session()->get('status')]);
     }
 }
