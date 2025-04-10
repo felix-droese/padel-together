04770e80bdbaab539a1f96d7c81cc22e490f31dd
felix-droese <felix.droese@clockin.de>
2025-04-10 19:54:58 +0200

    wip

--- resources/js/components/games/card/TeamDisplay.vue
+++ resources/js/components/games/card/TeamDisplay.vue
@@ -10,28 +10,29 @@ function getEloChange(playerId: number) {
 </script>
 
 <template>
-    <div class="w-fit md:w-[200px]">
-        <div v-for="player in props.team.players" :key="player.id" class="grid grid-cols-[1fr_auto] items-start gap-2 text-xs md:text-base">
+    <div class="w-full md:w-[200px]">
+        <div v-for="player in props.team.players" :key="player.id" class="flex items-start justify-between gap-6 text-xs md:text-base">
             <div class="mb-4 flex items-start gap-1 truncate">
                 <div class="flex flex-col">
                     <div class="flex items-center gap-1">
                         <span class="truncate">{{ player.last_name }}</span>
-                        <span v-if="player.elo" class="text-muted-foreground">{{ player.elo }}</span>
                     </div>
                     <span class="text-xs md:text-base">{{ player.first_name }}</span>
                 </div>
             </div>
-            <span
-                v-if="getEloChange(player.id)"
-                :class="{
-                    'text-green-600': (getEloChange(player.id)?.change ?? 0) > 0,
-                    'text-red-600': (getEloChange(player.id)?.change ?? 0) < 0,
-                }"
-                class="font-medium"
-            >
-                {{ (getEloChange(player.id)?.change ?? 0) > 0 ? '+' : '' }}
-                {{ getEloChange(player.id)?.change }}
-            </span>
+            <div class="flex flex-col">
+                <span v-if="player.elo" class="text-xs md:text-base">{{ player.elo }}</span>
+                <span
+                    v-if="player.elo"
+                    :class="{
+                        'text-green-600': (getEloChange(player.id)?.change ?? 0) > 0,
+                        'text-red-600': (getEloChange(player.id)?.change ?? 0) < 0,
+                    }"
+                    class="self-end"
+                >
+                    {{ (getEloChange(player.id)?.change ?? 0) > 0 ? '+' : '' }} {{ getEloChange(player.id)?.change }}</span
+                >
+            </div>
         </div>
     </div>
 </template>
